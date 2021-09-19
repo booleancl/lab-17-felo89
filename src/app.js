7084e5280ed78b3ca3f31d73b8daf6080b595f23
Felipe Pincheira <f.pincheira89@gmail.com>
2021-09-19 20:36:47 -0300

    added linter and fix audits

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
-const express = require("express");
+const express = require('express')
 
 // refactorizar
-const artistsController = require("./controllers/artists.controller");
+const artistsController = require('./controllers/artists.controller')
 
-const router = express.Router();
+const router = express.Router()
 
-router.get("/api/v1/artists", artistsController.getAllArtists);
-router.post("/api/v1/artists", artistsController.saveArtist);
-router.put("/api/v1/artists/:id", artistsController.updateArtist);
-router.delete("/api/v1/artists/:id", artistsController.removeArtist);
+router.get('/api/v1/artists', artistsController.getAllArtists)
+router.post('/api/v1/artists', artistsController.saveArtist)
+router.put('/api/v1/artists/:id', artistsController.updateArtist)
+router.delete('/api/v1/artists/:id', artistsController.removeArtist)
 
-const app = express();
-app.use(express.json());
-app.use(router);
+const app = express()
+app.use(express.json())
+app.use(router)
 
-module.exports = app;
+module.exports = app
